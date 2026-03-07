7cb4dd30d372c366faab9d3827f5ff6eea995e2b
pociclone-wq <pociclone@gmail.com>
2026-03-08 04:51:53 +0700

    Update Animation.lua

--- Animation.lua
+++ Animation.lua
@@ -1,1 +1,42 @@
+-- [[ DATA ANIMASI POCI.FISHIT ]] --
+-- Upload script ini ke: https://raw.githubusercontent.com/pociclone-wq/valentinefish/refs/heads/main/Animation.lua
 
+local EmotesData = {
+    {
+        Name = "Aura Backflip",
+        AnimationId = "rbxassetid://108299660040407",
+        AnimationPriority = Enum.AnimationPriority.Action3,
+        PlaybackSpeed = 1,
+        Looped = true
+    },
+    {
+        Name = "Cupid Bow Pose",
+        AnimationId = "rbxassetid://114493984403163",
+        AnimationPriority = Enum.AnimationPriority.Action2,
+        PlaybackSpeed = 1,
+        Looped = true
+    },
+    {
+        Name = "Cute Idle",
+        AnimationId = "rbxassetid://78087615433339",
+        AnimationPriority = Enum.AnimationPriority.Action3,
+        PlaybackSpeed = 1,
+        Looped = true
+    },
+    {
+        Name = "Fish Flop",
+        AnimationId = "rbxassetid://128069890626621",
+        AnimationPriority = Enum.AnimationPriority.Action2,
+        PlaybackSpeed = 1,
+        Looped = true
+    },
+    {
+        Name = "Midair Relax",
+        AnimationId = "rbxassetid://74898941129261",
+        AnimationPriority = Enum.AnimationPriority.Action2,
+        PlaybackSpeed = 1,
+        Looped = true
+    }
+}
+
+return EmotesData
